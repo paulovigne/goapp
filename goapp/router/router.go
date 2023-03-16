827de7c88f235a2cb8539c9a4f34ce02d2abc846
paulovigne <57726813+paulovigne@users.noreply.github.com>
2023-03-16 13:08:50 -0300

    Update router.go

--- goapp/router/router.go
+++ goapp/router/router.go
@@ -11,6 +11,8 @@ func Router() *mux.Router {
 
     router := mux.NewRouter()
 
+    router.Use(commonMiddleware)
+    
     router.HandleFunc("/api/user/{id}", middleware.GetUser).Methods("GET", "OPTIONS")
     router.HandleFunc("/api/user", middleware.GetAllUser).Methods("GET", "OPTIONS")
     router.HandleFunc("/api/newuser", middleware.CreateUser).Methods("POST", "OPTIONS")
@@ -18,4 +20,11 @@ func Router() *mux.Router {
     router.HandleFunc("/api/deleteuser/{id}", middleware.DeleteUser).Methods("DELETE", "OPTIONS")
 
     return router
+}
+
+func commonMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Add("Content-Type", "application/json")
+        next.ServeHTTP(w, r)
+    })
 }
